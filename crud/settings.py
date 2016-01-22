4bfb0336076b7dfc33adf8f2dec85d79bf819ad8
Martín Gerardo Suárez Hernández <gerardp.aries@gmail.com>
2016-01-21 22:16:20 -0600

    SQLite3
    CRUD en sqlite3 añadido

--- crud/settings.py
+++ crud/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'crud.wsgi.application'
 DATABASES = {
     'default': {
         #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'NAME': os.path.join(BASE_DIR, 'crud.sqlite3'),
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME':'crud',
         'USER':'postgres',
